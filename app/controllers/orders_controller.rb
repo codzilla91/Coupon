d0e9b9f7d15953563577540c0f56b886547eeef1
James <jtung501@gmail.com>
2023-10-05 21:14:25 +0800

    feat: added orders controller

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,38 @@
+class OrdersController < ApplicationController
+  # app/controllers/orders_controller.rb
+class OrdersController < ApplicationController
+  def new
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.save
+      # Apply the coupon discount if a valid coupon code is provided
+      if params[:coupon_code].present?
+        books_coupon = BooksCoupon.find_by(code: params[:coupon_code])
+        if books_coupon&.available?
+          @order.total_amount = books_coupon.apply_discount(@order.total_amount)
+          books_coupon.increment!(:used_count)
+        else
+          flash[:alert] = 'Invalid or expired coupon code.'
+          render :new
+          return
+        end
+      end
+
+      @order.save
+      redirect_to root_path, notice: 'Order placed successfully.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:total_amount)
+  end
+end
+
+end
